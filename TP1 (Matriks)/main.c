6b55fe9ed58a8043ab28166c4d8fa0d098644db9
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-19 08:01:39 +0700

    Delete TP1 (Matriks)/main.c

--- TP1 (Matriks)/main.c
+++
@@ -1,7 +0,0 @@
-/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
-mengerjakan tugas praktikum 1 dalam mata kuliah
-Algoritma dan Struktur Data untuk keberkahan-Nya,
-maka saya tidak akan melakukan kecurangan seperti
-yang telah di spesifikasikan Aamiin.*/
-#include "JRA25.h" // include library "JRA25.h"
-int main(){slv return 0;} // call macro slv and slove the program
