d2c48251da055edf67bfd9a2e21a622ac800eecf
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-24 05:15:14 +0700

    Delete TP2 (List Tunggal Dinamis)/main.c

--- TP2 (List Tunggal Dinamis)/main.c
+++
@@ -1,7 +0,0 @@
-/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
-mengerjakan tugas praktikum 2 dalam mata kuliah
-Algoritma dan Struktur Data untuk keberkahan-Nya,
-maka saya tidak akan melakukan kecurangan seperti
-yang telah di spesifikasikan Aamiin.*/
-#include "MDS25.h" // include library "MDS25.h"
-int main(){slv return 0;} // call macro slv and slove the program
