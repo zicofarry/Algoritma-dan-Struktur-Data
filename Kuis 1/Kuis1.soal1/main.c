a4e64441b6d3bd361db989e590482877786ff639
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-25 06:21:18 +0700

    Add files via upload

---
+++ Kuis 1/Kuis1.soal1/main.c
@@ -0,0 +1,8 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Kuis 1 soal 1 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+
+#include "mtbps25.h"
+int main(){slv return 0;} // call marco slv and solve the program
