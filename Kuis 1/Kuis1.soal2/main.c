b84dd07e7574c90ebe13146b1d2cc7a50736fb6c
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-28 04:19:34 +0700

    Add files via upload

---
+++ Kuis 1/Kuis1.soal2/main.c
@@ -0,0 +1,7 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Kuis 1 soal 2 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "lumwp25.h" // include my header -> lumwp25.h
+int main(){slv return 0;} // call macro slv and solve the program
