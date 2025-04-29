45dcdc4f6e4df516d2a51152a08cf17504ca8ff5
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-30 05:59:43 +0700

    Add files via upload

---
+++ UTS/UTS.soal1/main.c
@@ -0,0 +1,15 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan UTS soal 1 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+
+#include "prtk25.h" // include header "prtk25.h"
+
+/*Overall, this program mostly uses Big O(n) or linear,
+but in addFirst procedure, we have to check whether the list can be
+added or not, by checking the capacity and in that process it will
+loop until the last element, so that we get the number element of the
+list, and in that process, the program uses Big O(n^2) or exponential*/
+
+int main(){slv return 0;} // call macro slv and solve the program
