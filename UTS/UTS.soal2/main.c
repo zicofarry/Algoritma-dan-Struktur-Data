c0a23997dc377196b28998763c82c6ac2e3ec51a
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-05-03 08:31:08 +0700

    Add files via upload

---
+++ UTS/UTS.soal2/main.c
@@ -0,0 +1,16 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan UTS soal 2 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+
+#include "orrr25.h" // include header "orrr25.h"
+
+/*in so many procedure, this program uses more Big O(n) or linear,
+and in the procedure solve (my main) this program uses more Big O(n^2)
+or exponential,but in some cases, when the row is not found, we need
+to addLastR and search for the row element again,
+so the search for the row element will be done 2 times,
+and in that process, the program uses Big O(n*(n+2))*/
+
+int main(){slv return 0;} // call macro slv and solve the program
