b0cff8076dbe524e3220d3cbe0309b79b3e39a58
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-05-13 09:58:02 +0700

    Add files via upload

---
+++ Kuis 2/Kuis2.soal1/main.c
@@ -0,0 +1,12 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Kuis 2 soal 1 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "stpak25.h" // include my header "stpak25.h"
+int main(){slv return 0;} // call macro slv and solve the program
+/* in this problem, i didnt find any greedy choices, but if we do
+cocokologi, the greedy choice that we can use to solve this problem
+is we can just see the date, if the date was bigger, we could move 
+from Stack Utama to Stack Temp 1, if its not just move from Stack
+Utama to Stack Temp 2 */
