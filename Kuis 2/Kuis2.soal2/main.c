3773391b440550953224f17e6d75bec44c984368
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-05-19 04:08:31 +0700

    Add files via upload

---
+++ Kuis 2/Kuis2.soal2/main.c
@@ -0,0 +1,12 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Kuis 2 soal 2 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "qptim25.h" // include header "qptim25.h"
+int main(){slv return 0;} // call macro slv and solve the program
+/* in this problem, the best greedy choice is we could make procedure
+to add element based on priority, we didnt need to add element normally
+and do a sorting because its take a more process and of course its not
+efficient. So with addPriority procedure we can choose where we want to
+place the index element of the queue, cuz its more efficient */
