5246555c931fcf67b2435819941b02a4c4ee7acd
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-05-28 01:48:22 +0700

    Add files via upload

---
+++ TP3 (List Ganda Statis)/DCO25/main.c
@@ -0,0 +1,7 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Tugas Praktikum 3 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "DCO25.h" // include library "DCO25.h"
+int main(){slv return 0;} // call macro slv and solve the program
