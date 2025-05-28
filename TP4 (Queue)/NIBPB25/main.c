fab19e8f31f222e7168bc5a2b061241c005fb748
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-05-29 01:01:28 +0700

    Add files via upload

---
+++ TP4 (Queue)/NIBPB25/main.c
@@ -0,0 +1,7 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan tugas praktikum 4 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "NIBPB25.h" // include header "NIBPB25.h"
+int main(){slv return 0;} // call macro slv and solve the program
