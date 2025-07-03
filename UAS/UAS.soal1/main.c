575aa61ac5f8dcb6d76bfb9f081c0da8f6da1e77
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-04 04:21:54 +0700

    Add files via upload

---
+++ UAS/UAS.soal1/main.c
@@ -0,0 +1,7 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan UAS Soal 1 dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "pbhll25.h" // include header "pbhll25.h"
+int main(){slv return 0;} // call macro slv and solve the program
